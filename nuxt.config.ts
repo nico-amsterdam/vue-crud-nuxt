78e352879930d9d4204bcdd5c62153c2dd5dfb89
nico-amsterdam <n.g.c.hoogervort@gmail.com>
2024-11-20 23:02:37 +0100

    Applied uncss to boodstrap 3 + theme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -32,15 +32,18 @@ export default defineNuxtConfig({
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         { hid: 'description', name: 'description', content: 'Vue CRUD Nuxt.js project' }
-      ],
-      link: [
-          { rel:  'stylesheet prefetch', 
-            href: 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css' }
-        , { rel:  'stylesheet prefetch',
-            href: 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap-theme.min.css'}
       ]
     }    
   },
+  css: [
+    // bootstrap3 + theme via uncss
+    '~/assets/css/bootstrap3-un.css'
+  ],
+  experimental: {
+    inlineSSRStyles: true
+  },
+
+  addMeta: true,
 
   imports: {
     dirs: ['stores']
