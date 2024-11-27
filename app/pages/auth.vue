f2198537deebd0722f2eeb3e6d358070011f0f44
nico-amsterdam <n.g.c.hoogervort@gmail.com>
2024-11-27 15:51:50 +0100

    ogin page in /auth path

--- app/pages/auth.vue
+++ app/pages/auth.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import { Icon } from '@iconify/vue'
 
+const username = ref(''), name = ref('')
+const errors = ref([{ message: 'Foo' }, { message: 'Bar' }])
+
 useHead({
     bodyAttrs: { class: 'auth' }
   , link: [{ rel: 'stylesheet', href: '/_nuxt/assets/css/auth.css' }]
@@ -14,15 +17,20 @@ useHead({
                 <h1 class="column">Vue crud Nuxt demo</h1>
                 <Icon icon="tabler:sun" :ssr="true" title="Dark mode swith" class="darkmode" />
             </header>
+            <ul class="errors flash">
+                <li v-for="error in errors">
+                    {{ error.message }}
+                </li>
+            </ul>
             <div class="row signup_or_signin">
                 <article class="column signup">
                     <h2>Sign Up</h2>
                     <form>
                         <div class="input-group">
-                            <input type="text" placeholder="Username" required>
+                            <input type="text" v-model="username" placeholder="Username" id="signup-username" required>
                         </div>
                         <div class="input-group">
-                            <input type="text" placeholder="Full Name" required>
+                            <input type="text" v-model="name" placeholder="Full Name" id="signup-name" required>
                         </div>
                         <button type="submit">Sign Up</button>
                     </form>
@@ -36,7 +44,7 @@ useHead({
                     <h2>Sign In</h2>
                     <form>
                         <div class="input-group">
-                            <input type="text" placeholder="Username" required>
+                            <input type="text" v-model="username" placeholder="Username" id="signin-username" required>
                         </div>
                         <button type="submit">Sign In</button>
                     </form>
