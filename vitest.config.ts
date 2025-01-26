788855aee7b9c475088adcb05e91e35d93420334
nico-amsterdam <n.g.c.hoogervort@gmail.com>
2025-01-26 15:28:18 +0100

    Direct Vue dependency needed for vitest vitejs/plugin-vue

--- vitest.config.ts
+++ vitest.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '~': fileURLToPath(new URL('./', import.meta.url)),
-      '@': fileURLToPath(new URL('./app', import.meta.url) + '/'),
+      '@': fileURLToPath(new URL('./app', import.meta.url)) + '/',
     },
   },
 })
