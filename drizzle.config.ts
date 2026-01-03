57afa0a49c00eadd853867bc8e5408d02318f095
nico-amsterdam <n.g.c.hoogervort@gmail.com>
2026-01-03 14:34:38 +0100

    Update README and some script name changes

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
   dbCredentials: {
       accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
       databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
-      token: process.env.CLOUDFLARE_TOKEN!,
+      token: process.env.CLOUDFLARE_API_TOKEN!,
   },
 })
